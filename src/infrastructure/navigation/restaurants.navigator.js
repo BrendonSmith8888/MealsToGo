c5fcedf13a1ca56306e7f421ba20064b97d17d55
Brendon Smith <brendonsmith8888@gmail.com>
2021-11-23 16:52:12 +0200

    Update

--- src/infrastructure/navigation/restaurants.navigator.js
+++ src/infrastructure/navigation/restaurants.navigator.js
@@ -8,7 +8,7 @@ const RestaurantStack = createStackNavigator();
 
 export const RestaurantsNavigator = () => {
   return (
-    <RestaurantStack.Navigator headerMode="none">
+    <RestaurantStack.Navigator headerShown="false">
       <RestaurantStack.Screen
         name="Restaurants"
         component={RestaurantsScreen}
