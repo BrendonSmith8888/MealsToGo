6770bbedc059fa18bc6384350257d2eb1cc58d30
Brendon Smith <brendonsmith8888@gmail.com>
2021-11-30 20:08:58 +0200

    Update

--- src/services/authentication/authentication.context.js
+++ src/services/authentication/authentication.context.js
@@ -1,5 +1,6 @@
 import React, { useState, createContext } from "react";
 import * as firebase from "firebase";
+
 import { loginRequest } from "./authentication.service";
 
 export const AuthenticationContext = createContext();
@@ -8,6 +9,15 @@ export const AuthenticationContextProvider = ({ children }) => {
   const [isLoading, setIsLoading] = useState(false);
   const [user, setUser] = useState(null);
   const [error, setError] = useState(null);
+
+  firebase.auth().onAuthStateChanged((usr) => {
+    if (usr) {
+      setUser(usr);
+      setIsLoading(false);
+    } else {
+      setIsLoading(false);
+    }
+  });
 
   const onLogin = (email, password) => {
     setIsLoading(true);
@@ -18,13 +28,45 @@ export const AuthenticationContextProvider = ({ children }) => {
       })
       .catch((e) => {
         setIsLoading(false);
-        setError(e);
+        setError(e.toString());
       });
+  };
+
+  const onRegister = (email, password, repeatedPassword) => {
+    setIsLoading(true);
+    if (password !== repeatedPassword) {
+      setError("Error: Passwords do not match");
+      return;
+    }
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword(email, password)
+      .then((u) => {
+        setUser(u);
+        setIsLoading(false);
+      })
+      .catch((e) => {
+        setIsLoading(false);
+        setError(e.toString());
+      });
+  };
+
+  const onLogout = () => {
+    setUser(null);
+    firebase.auth().signOut();
   };
 
   return (
     <AuthenticationContext.Provider
-      value={{ isAuthenticated: !!user, user, isLoading, error, onLogin }}
+      value={{
+        isAuthenticated: !!user,
+        user,
+        isLoading,
+        error,
+        onLogin,
+        onRegister,
+        onLogout,
+      }}
     >
       {children}
     </AuthenticationContext.Provider>
